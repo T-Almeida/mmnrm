ed94f796daa30aa06f2912090cfdefaf4f6ab9e4
Tiago Almeida <tiagomeloalmeida@ua.pt>
2020-09-28 15:51:30 +0000

    add load function

--- mmnrm/utils.py
+++ mmnrm/utils.py
@@ -26,6 +26,75 @@ def load_model_weights(file_name, model):
             weight.append(f['weight'+str(i)][:])
         model.set_weights(weight)
         
+def load_neural_model(path_to_weights):
+    
+    rank_model = load_model(path_to_weights, change_config={"return_snippets_score":True})
+    tk = rank_model.tokenizer
+    
+    model_cfg = rank_model.savable_config["model"]
+    
+    max_input_query = model_cfg["max_q_length"]
+    max_input_sentence = model_cfg["max_s_length"]
+    max_s_per_q_term = model_cfg["max_s_per_q_term"]
+    
+    # redundant code... replace
+    max_sentences_per_query = model_cfg["max_s_per_q_term"]
+
+    pad_query = lambda x, dtype='int32': tf.keras.preprocessing.sequence.pad_sequences(x, 
+                                                                                       maxlen=max_input_query,
+                                                                                       dtype=dtype, 
+                                                                                       padding='post', 
+                                                                                       truncating='post', 
+                                                                                       value=0)
+
+    pad_sentences = lambda x, dtype='int32': tf.keras.preprocessing.sequence.pad_sequences(x, 
+                                                                                           maxlen=max_input_sentence,
+                                                                                           dtype=dtype, 
+                                                                                           padding='post', 
+                                                                                           truncating='post', 
+                                                                                           value=0)
+
+    pad_docs = lambda x, max_lim, dtype='int32': x[:max_lim] + [[]]*(max_lim-len(x))
+
+    idf_from_id_token = lambda x: math.log(tk.document_count/tk.word_docs[tk.index_word[x]])
+
+    train_sentence_generator, test_sentence_generator = sentence_splitter_builderV2(tk, 
+                                                                                      max_sentence_size=max_input_sentence,
+                                                                                      mode=4)
+    
+    def test_input_generator(data_generator):
+
+        data_generator = test_sentence_generator(data_generator)
+
+        for _id, query, docs in data_generator:
+
+            #tokenization
+            query_idf = list(map(lambda x: idf_from_id_token(x), query))
+
+            tokenized_docs = []
+            ids_docs = []
+            offsets_docs = []
+
+            for doc in docs:
+
+                padded_doc = pad_docs(doc["text"], max_lim=max_input_query)
+                for q in range(len(padded_doc)):
+                    padded_doc[q] = pad_docs(padded_doc[q], max_lim=max_sentences_per_query)
+                    padded_doc[q] = pad_sentences(padded_doc[q])
+                tokenized_docs.append(padded_doc)
+                ids_docs.append(doc["id"])
+                offsets_docs.append(doc["offset"])
+
+            # padding
+            query = pad_query([query])[0]
+            query = [query] * len(tokenized_docs)
+            query_idf = pad_query([query_idf], dtype="float32")[0]
+            query_idf = [query_idf] * len(tokenized_docs)
+
+            yield _id, [np.array(query), np.array(tokenized_docs), np.array(query_idf)], ids_docs, offsets_docs
+
+    return rank_model, test_input_generator
+        
 def save_model(file_name, model):
     cfg = model.savable_config
     with open(file_name+".cfg","wb") as f:
